18f8b82c1e40b8d8fb3ad4214331780268a8f3d5
xumeijie <xcdh560@foxmail.com>
2022-03-03 17:20:48 +0800

    default

--- src/network/request.js
+++ src/network/request.js
@@ -4,17 +4,18 @@ export function request(config) {
   // 创建实例
   const instance = axios.create({
     baseURL: 'http://123.207.32.32:8000',
+    // baseURL: 'http://adi-v3.dev',
     timeout: 5000
   });
-  
+
   // 请求拦截器
   instance.interceptors.request.use(config => {
-      return config;
-    },err => {
-      console.log(err);
-    }
+    return config;
+  }, err => {
+    console.log(err);
+  }
   );
-  
+
   // 响应拦截器
   instance.interceptors.response.use(res => {
     return res.data;
--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper" ref="aaaa">
+  <div class="wrapper" ref="wrapper">
     <ul class="content">
       <li>分类列表</li>
       <li>分类列表</li>
@@ -110,14 +110,24 @@ import BScroll from "better-scroll";
 
 export default {
   name: "Category",
-  data() {
-    return {
-      scroll: null,
-    };
-  },
+  // data() {
+  //   return {
+  //     scroll: null,
+  //   };
+  // },
   mounted() {
-    this.scroll = new BScroll(this.$refs.aaaa, {});
+    this.$nextTick(() => {
+      this.scroll = new BScroll(this.$refs.wrapper, {
+        probeType: 3,  // 1-1都是不能实时监听滚动的；2只监听手指的滚动；3只要是滚动全都监听
+        click: true,   // 默认阻止浏览器的点击事件，必须设置为true
+        pullUpLoad: true,
+      });
+    });
   },
+
+  methods: {
+    
+  }
 };
 </script>
 
@@ -125,7 +135,7 @@ export default {
 .content {
   height: 150px;
   background-color: bisque;
-  overflow: hidden;
-  /* overflow-y: scroll; */
+  /* /* overflow: hidden; */
+  overflow-y: scroll;
 }
 </style>
